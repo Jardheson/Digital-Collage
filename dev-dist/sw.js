45b69c9b1b7478a4d9bb9d04cb6e3de401c31849
Jardheson <herlles2022@gmail.com>
2026-02-27 23:40:01 -0300

    Tarefa: atualizar o ID do projeto Vercel e a revisão do service worker
    Atualize o ID do projeto no arquivo de configuração do Vercel para refletir a nova associação de projeto.
    
    A revisão do cache do service worker também é atualizada como parte do processo de build.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u03b6usn4m4"
+    "revision": "0.vcp3hg133gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
